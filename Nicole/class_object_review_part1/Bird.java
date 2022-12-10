c739f1c7b875e2ce25d33c06e9edceffc9164358
unknown <xul019@ucsd.edu>
2022-12-09 16:32:44 -0800

    .

---
+++ Nicole/class_object_review_part1/Bird.java
@@ -0,0 +1,37 @@
+class Bird {
+
+    public double weight_public = 2;
+    private double weight_private = 2;
+
+    private double weight;
+
+    // basically, you can use both private and public variable anywhere in the class
+    public double test_use = weight_private;
+
+    public static boolean living_condition_good_or_not = true;
+
+    // // use constructor to initialize instance variable
+    public Bird(double initial_weight) {
+        weight = initial_weight;
+    }
+
+    // this method allow object to use private variable
+    public double get_weight() {
+        // see, you can use private variable anywhere inside the class
+        return weight;
+    }
+
+    public void eat(double amount) {
+        weight = weight + amount;
+    }
+
+    // private double square(double x) {
+    //     // f(x) = x^2
+    //     return x*x;
+    // }
+
+
+    public static void change_living_condition() {
+        living_condition_good_or_not = !living_condition_good_or_not;
+    }
+}
